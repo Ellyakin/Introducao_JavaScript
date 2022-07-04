0e774e8f8ec7bbbd8bc4e0ad165b98ebfa553b66
Ellyakin <ellyakinjsf@gmail.com>
2022-07-04 19:50:32 -0400

    primeira aula JavaScript Manipulando Variaveis

---
+++ aula1-Manipulando arquivos.js
@@ -0,0 +1,17 @@
+
+// var y=0;
+/*comentario com mais de uma lina
+comentario continua*/
+
+// ETAPA 1
+var preco= 2;
+var desconto=0.2;
+// variavel pode ser manipulada
+preco= preco- desconto
+
+//ETAPA 2
+const PRECO2 = 2; // CONSTANTE O VALOR NÃO PODE SER MANIPULADO
+var total = PRECO - desconto
+
+
+
